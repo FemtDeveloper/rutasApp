a388a5f34197dc724bcaff324521520b0e6c1ce5
Felix Miranda <feedmite@hotmail.com>
2023-04-13 00:32:34 -0500

    routeMaps main finished

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,17 +1,46 @@
-import React, {useRef} from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import {StyleSheet} from 'react-native';
-import MapView, {Marker} from 'react-native-maps';
+import MapView, {Marker, Polyline} from 'react-native-maps';
 import {useLocation} from '../hooks/useLocation';
 import {LoadingScreen} from '../pages/LoadingScreen';
 import {Fab} from './Fab';
 
 export const Map = () => {
-  const {hasLocation, initialPosition, getCurrentLocation} = useLocation();
+  const [showPolilyne, setShowPolilyne] = useState(false);
+  const {
+    hasLocation,
+    initialPosition,
+    userLocation,
+    routeLine,
+    getCurrentLocation,
+    followUserLocation,
+    stopFollowUserLocation,
+  } = useLocation();
 
   const mapViewRef = useRef<MapView>();
+  const following = useRef<boolean>(true);
+
+  useEffect(() => {
+    followUserLocation();
+    return () => {
+      stopFollowUserLocation();
+    };
+  }, []);
+
+  useEffect(() => {
+    if (!following.current) {
+      return;
+    }
+    const {latitude, longitude} = userLocation;
+    mapViewRef.current?.animateCamera({center: {latitude, longitude}});
+
+    return () => {};
+  }, [userLocation]);
 
   const centerPosition = async () => {
     const {longitude, latitude} = await getCurrentLocation();
+
+    following.current = true;
 
     mapViewRef.current?.animateCamera({center: {latitude, longitude}});
   };
@@ -30,7 +59,8 @@ export const Map = () => {
           longitude: initialPosition.longitude,
           latitudeDelta: 0.015,
           longitudeDelta: 0.0121,
-        }}>
+        }}
+        onTouchStart={() => (following.current = false)}>
         <Marker
           image={require('../assets/custom-marker.png')}
           coordinate={{latitude: 37.78825, longitude: -122.4324}}
@@ -39,11 +69,18 @@ export const Map = () => {
           style={styles.marker}
         />
       </MapView>
+      {showPolilyne && <Polyline coordinates={routeLine} />}
       <Fab
         iconName="star-outline"
         onPress={centerPosition}
         // eslint-disable-next-line react-native/no-inline-styles
         style={{position: 'absolute', bottom: 40, right: 40}}
+      />
+      <Fab
+        iconName="brush-outline"
+        onPress={() => setShowPolilyne(prev => !prev)}
+        // eslint-disable-next-line react-native/no-inline-styles
+        style={{position: 'absolute', bottom: 80, right: 40}}
       />
     </>
   );
--- src/hooks/useLocation.ts
+++ src/hooks/useLocation.ts
@@ -1,16 +1,38 @@
 import Geolocation from '@react-native-community/geolocation';
-import {useEffect, useState} from 'react';
+import {useEffect, useRef, useState} from 'react';
 import {Location} from '../interfaces/appInterfaces';
 
 export const useLocation = () => {
   const [hasLocation, setHasLocation] = useState(false);
+  const [routeLine, setRouteLine] = useState<Location[]>([]);
   const [initialPosition, setInitialPosition] = useState<Location>({
     latitude: 0,
     longitude: 0,
   });
+  const [userLocation, setUserLocation] = useState<Location>({
+    latitude: 0,
+    longitude: 0,
+  });
+
+  const watchRef = useRef<number>();
+  const isMounted = useRef(true);
+
+  useEffect(() => {
+    isMounted.current = true;
+
+    return () => {
+      isMounted.current = false;
+    };
+  }, []);
+
   useEffect(() => {
     getCurrentLocation().then(location => {
+      if (!isMounted.current) {
+        return;
+      }
       setInitialPosition(location);
+      setUserLocation(location);
+      setRouteLine(prev => [...prev, location]);
       setHasLocation(true);
     });
   }, []);
@@ -30,5 +52,38 @@ export const useLocation = () => {
     });
   };
 
-  return {hasLocation, initialPosition, getCurrentLocation};
+  const followUserLocation = () => {
+    watchRef.current = Geolocation.watchPosition(
+      ({coords}) => {
+        if (!isMounted.current) {
+          return;
+        }
+        const location: Location = {
+          latitude: coords.latitude,
+          longitude: coords.longitude,
+        };
+        setUserLocation({
+          latitude: coords.latitude,
+          longitude: coords.longitude,
+        });
+        setRouteLine(prev => [...prev, location]);
+      },
+      err => console.error({err}),
+      {enableHighAccuracy: true, distanceFilter: 10},
+    );
+  };
+
+  const stopFollowUserLocation = () => {
+    Geolocation.clearWatch(watchRef.current!);
+  };
+
+  return {
+    hasLocation,
+    initialPosition,
+    userLocation,
+    routeLine,
+    getCurrentLocation,
+    followUserLocation,
+    stopFollowUserLocation,
+  };
 };
